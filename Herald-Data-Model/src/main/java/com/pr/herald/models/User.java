1c461d2eb1dc6d7b2462b2c2266aa500db54b636
pratik <pratiksonii@yahoo.in>
2016-08-27 18:42:18 +0530

    Project created with Data models

---
+++ Herald-Data-Model/src/main/java/com/pr/herald/models/User.java
@@ -0,0 +1,36 @@
+package com.pr.herald.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+@Table(name = "User")
+public class User {
+	
+	private Long id;
+	private String name;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.SEQUENCE)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
